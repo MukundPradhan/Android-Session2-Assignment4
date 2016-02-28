5a02f8eede432f52b6f46d36e298271736231b6a
MukundPradhan <replyme.mmp@gmail.com>
2016-02-29 00:07:15 +0530

    Initial commit

---
+++ app/src/main/java/com/assignments/acadgild/session2_assignment4/MainActivity.java
@@ -0,0 +1,16 @@
+package com.assignments.acadgild.session2_assignment4;
+
+import android.content.pm.ActivityInfo;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+    }
+}
